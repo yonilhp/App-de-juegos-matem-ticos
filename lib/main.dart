ade2f64c06cbec9d45a83e3c267dc7426355946e
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2026-01-09 14:50:51 -0500

    Conexión a Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:num_num/firebase_options.dart';
 
-void main() {
+void main() async {
+  // Esto asegura que se inicialicen los bindings de Flutter antes de usar cualquier
+  // funcionalidad que dependa de ellos, como Firebase.
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
